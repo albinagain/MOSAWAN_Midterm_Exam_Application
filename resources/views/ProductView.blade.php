efbacdedc98e9590f1826ca3ff3c1f45957085ed
ajmmosawan@gmail.com <ajmmosawan@gmail.com>
2024-02-21 09:48:06 +0800

    added CSS through Bootstrap

--- resources/views/ProductView.blade.php
+++ resources/views/ProductView.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Product</title>
 </head>
 <body>
-    <div>
+    <div class="container text-center">
         <h1>List of iPhones</h1>
     </div>
-    <div>
+    <div class="container">
         <ul>
             @foreach ($product as $item)
                 <li>{{$item}}</li>
